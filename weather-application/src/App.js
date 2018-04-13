c076c0575873e081653dcd156808976d8ab0359d
Sarang Sawant <ssawant5@binghamton.edu>
2018-04-13 13:43:09 -0400

    contruct open weather map url with query params

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const APP_KEY = "a2e6ffc5c58e173911390e3a96b3bb68";
 
 class App extends React.Component {
+
+    getWeather = async(e) => {
+        e.preventDefault();
+        console.log("Here");
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid="+ APP_KEY;
+        const api_call = await fetch(url);
+
+        const data = await api_call.json();
+        console.log(data);
+    } 
     render() {
         return (
-            <div > Hello World</div>
+            <div>
+                <Titles />
+                <Form getWeather={this.getWeather} />
+                <Weather />
+            </div>
         );
     }
 };
