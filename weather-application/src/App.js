80b249b06a8e993ff72032c12b886a354c05f7a0
Sarang Sawant <ssawant5@binghamton.edu>
2018-04-14 00:54:29 -0400

    Css and bootstrap Styling added

--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
 
-        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid="+ APP_KEY;
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid="+ APP_KEY + "&units=metric";
 
         if (city && country) {
             const api_call = await fetch(url);
@@ -51,19 +51,33 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Titles />
-                <Form getWeather={this.getWeather} />
-                <Weather
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                />
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-5 title-container">
+                                    <Titles />
+                                </div>
+
+                                <div className="col-xs-7 form-container">
+                                    <Form getWeather={this.getWeather} />
+                                    <Weather
+                                        temperature={this.state.temperature}
+                                        city={this.state.city}
+                                        country={this.state.country}
+                                        humidity={this.state.humidity}
+                                        description={this.state.description}
+                                        error={this.state.error}
+                                    />
+                                </div>
+
+                            </div>    
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 };
-
+  
 export default App;
